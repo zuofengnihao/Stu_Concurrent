1f8333026cc19c91f0b2033e66720f8852190fe0
zuofeng <zuofengnihao@163.com>
2020-10-14 09:36:21 +0800

    a

---
+++ src/stuconcurrent/chapter4/ThreadState.java
@@ -0,0 +1,50 @@
+package stuconcurrent.chapter4;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        new Thread(new TimeWaiting(), "TimeWaitingThread").start();
+        new Thread(new Waiting(), "WaitingThread").start();
+        new Thread(new Blocked(), "BlockedThread-1").start();
+        new Thread(new Blocked(), "BlockedThread-2").start();
+    }
+
+    // 该线程不断进行睡眠
+    static class TimeWaiting implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtils.second(100);
+            }
+        }
+    }
+
+    // 该线程在Waiting.class实例上等待
+    static class Waiting implements Runnable {
+
+        @Override
+        public void run() {
+            synchronized (Waiting.class) {
+                try {
+                    Waiting.class.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 该线程在Blocked.class实例上加锁后，不会释放该锁
+    static class Blocked implements Runnable {
+
+        @Override
+        public void run() {
+            synchronized (Blocked.class) {
+                while (true) {
+                    SleepUtils.second(100);
+                }
+            }
+        }
+    }
+}
