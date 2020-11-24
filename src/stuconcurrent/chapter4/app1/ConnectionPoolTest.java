32d83596fa1742ae034b1b482af907613113c695
zuofeng <zuofengnihao@163.com>
2020-11-25 00:14:47 +0800

    home

---
+++ src/stuconcurrent/chapter4/app1/ConnectionPoolTest.java
@@ -0,0 +1,74 @@
+package stuconcurrent.chapter4.app1;
+
+import java.sql.Connection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ConnectionPoolTest {
+    static ConnectionPool pool = new ConnectionPool(10);
+    // 保证所有ConnectionRunner能够同时开始
+    static CountDownLatch start = new CountDownLatch(1);
+    //main线程将会等待所有ConnectionRunner结束后才能继续执行
+    static CountDownLatch end;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 线程数量，可以修改数量观察
+        int threadCount = 10;
+        end = new CountDownLatch(threadCount);
+        int count = 20;
+        AtomicInteger got = new AtomicInteger();
+        AtomicInteger notGot = new AtomicInteger();
+        for (int i = 0; i < threadCount; i++) {
+            Thread thread = new Thread(new ConnectionRunner(count, got, notGot), "ConnectionRunnerThread");
+            thread.start();
+        }
+        start.countDown();
+        end.await();
+        System.out.println("total invoke: " + (threadCount * count));
+        System.out.println("got connection: " + got);
+        System.out.println("not got connection: " + notGot);
+    }
+
+    static class ConnectionRunner implements Runnable {
+        int count;
+        AtomicInteger got;
+        AtomicInteger notGot;
+
+        public ConnectionRunner(int count, AtomicInteger got, AtomicInteger notGot) {
+            this.count = count;
+            this.got = got;
+            this.notGot = notGot;
+        }
+
+        @Override
+        public void run() {
+            try {
+                start.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            while (count > 0) {
+                try {
+                    // 从线程池中获取连接，如果1000毫秒内无法获得，将会返回null
+                    Connection connection = pool.fetchConnection(1000);
+                    if (connection != null) {
+                        try {
+                            connection.createStatement();
+                            connection.commit();
+                        } finally {
+                            pool.releaseConnection(connection);
+                            got.incrementAndGet();
+                        }
+                    } else {
+                        notGot.incrementAndGet();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    count--;
+                }
+                end.countDown();
+            }
+        }
+    }
+}
